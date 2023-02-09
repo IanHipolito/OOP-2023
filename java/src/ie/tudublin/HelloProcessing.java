c30f3c727390ff0cfabce27ee1d0eeb238ca5cf3
IanHipolito <ianhipolito04@gmail.com>
2023-02-09 15:27:46 +0000

    monday2

--- java/src/ie/tudublin/HelloProcessing.java
+++ java/src/ie/tudublin/HelloProcessing.java
@@ -12,14 +12,33 @@ public class HelloProcessing extends PApplet
 
 	public void setup() 
 	{
-		colorMode(HSB);
-		background(0);
+		//colorMode(HSB);
+		background(255, 0, 0);
 	}
 
-	
-	
 	public void draw()
 	{	
-		
+		// stroke(255);
+		// line(10, 10, 100, 100); // x1, y1, x2, y2
+		// circle(300, 250, 70); // cx, cy, d
+		// fill(0, 255, 0); //red, green, blue
+		// rect(10, 300, 20, 100); //tlx, tly, w, h
+		// stroke(127);
+		// fill(0, 0, 255);
+		// noStroke();
+		// noFill();
+		// strokeWeight(1);
+		// triangle(40, 90, 300, 20, 80, 70);
+		// fill(0, 0, 255);
+
+		noStroke();
+		fill(255,255, 0);
+		circle(250, 250, 400);
+		fill(0, 255, 255);
+		triangle(250, 10, 50, 400, 450, 400);
+		fill(255, 255, 255);
+		ellipse(250, 200, 180, 100);
+		fill(0, 0, 0);
+		ellipse(250, 200, 60, mouseY);
 	}
 }
