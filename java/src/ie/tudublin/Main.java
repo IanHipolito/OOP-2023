eb2f9b1a7c6de909bcf66aa0327f1da3263afa21
IanHipolito <ianhipolito04@gmail.com>
2023-03-06 11:44:09 +0000

    Week6 Digital Audio 2

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -33,6 +33,12 @@ public class Main
         processing.core.PApplet.runSketch( a, new Audio1());
     }
 	
+	public static void audio2()
+	{
+		String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new Audio2());
+    }
+
 	public static void main(String[] args)
 	{
 		System.out.println("Hello world");
@@ -41,7 +47,7 @@ public class Main
 		penny.setName("Penny");
 		penny.speak();
 
-		audio1();
+		audio2();
 	}
 	
 }
